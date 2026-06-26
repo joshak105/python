e6080ddf5ed71753efc5f5de326e9cf2e7213b24
Joshua Akhabhau <ja105@kent.ac.uk>
2026-06-26 23:53:46 +0100

    Add files via upload

---
+++ Pink Morsel Sales.js
@@ -0,0 +1,19 @@
+const northButton = document.getElementById("northbutton");
+const southButton = document.getElementById("southbutton");
+const westButton = document.getElementById("westbutton");
+const eastButton = document.getElementById("eastbutton");
+
+northButton.addEventListener("click", function() {
+
+})
+southButton.addEventListener("click", function() {
+
+})
+westButton.addEventListener("click", function() {
+
+})
+eastButton.addEventListener("click", function() {}
+)
+westButton.addEventListener("click", function() {
+
+})
